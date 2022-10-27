3fe4eb7554f3a1a2b839dd4693280a1573f8b738
lilian-ARNAUDIES <ljb.arnaudies@gmail.com>
2022-10-27 10:17:04 +0200

    fix carte

--- public/js/map.js
+++ public/js/map.js
@@ -6,23 +6,36 @@ $(document).ready(function(){
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>'
     }).addTo(map);
 
-    let path = "{{ path('app_map')}}";
-    $.ajax({
-        type : 'POST',
-        url : path,
-        datatype : 'json',
-        success : function(response)
-        {
-            console.log(response);
-        },
-        error : function(){
-            console.log("marche pas")
-        }
-    })
+
+    function affichageGlobal()
+    {   
+        let myurl = $('#map').attr("data-path");
+
+        $.ajax({
+            type: 'POST',
+            url: myurl,
+            datatype: 'json',
+            data:{
+                'monstring' : 'youhou!'
+            },
+
+            success: function(response)
+            {
+                console.log(response)
+            },
+            error: function()
+            {
+                console.log('ça marche pas');
+            }
+        })
+    };
+    
+    affichageGlobal();
+    
     for(let i = 0; i>length(response); i++)
     {
         L.marker([response.latitude, response.longitude]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .bindPopup(response.type)
         .openPopup();
     }
 })
