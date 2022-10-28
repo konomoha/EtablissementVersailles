6fa41185382e8c65a23fd33452dc28f5fd79d22a
lilian-ARNAUDIES <ljb.arnaudies@gmail.com>
2022-10-28 09:47:43 +0200

    test filtres

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var map = L.map('map').setView([48.801408, 2.130122], 14);
+    var map = L.map('map').setView([48.801408, 2.130122], 13    );
     // console.log(map);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -34,13 +34,34 @@ $(document).ready(function(){
     affichageGlobal();
 
     function affichagePoint(response){
+        $(document).ready(function() {
+            $('#submit').click(function() {
+                let ecole = $("input[type=checkbox][name=inlineRadioOptions1]:checked").val();
+                let college = $("input[type=checkbox][name=inlineRadioOptions2]:checked").val();
+                let lycee = $("input[type=checkbox][name=inlineRadioOptions3]:checked").val();
+            })
+            var listeEcole = {}, prop;
+            response.forEach(element => {
+                if (response.type===ecole){
+                    var listeEcole = clone(response);
 
-        for(let i = 0; i < response.length; i++)
-        {
-            L.marker([response[i].latitude, response[i].longitude]).addTo(map)
-            .bindPopup(response[i].nom)
-            .openPopup();
+                }
+            });
+        });
+
+        for(let i = 0; i < listeEcole.length; i++)
+        {             
+                L.marker([listeEcole[i].latitude, listeEcole[i].longitude]).addTo(map)
+                .bindPopup(listeEcole[i].nom)
+                .openPopup();
         }
+        
+        // for(let i = 0; i < response.length; i++)
+        // {             
+        //         L.marker([response[i].latitude, response[i].longitude]).addTo(map)
+        //         .bindPopup(response[i].nom)
+        //         .openPopup();
+        // }
     }
-    
+
 });
